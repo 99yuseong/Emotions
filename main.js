437f49db35bdff666471352c51f8ad255772d6ea
99yuseong <dbtjd6511@gmail.com>
2022-12-22 09:31:26 +0900

    update whole changes

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const cubeBtn = document.querySelector(".cube");
-const waveBtn = document.querySelector(".wave");
+const tornadoBtn = document.querySelector(".tornado");
+const happyBtn = document.querySelector(".happy");
+const angerBtn = document.querySelector(".anger");
+const sadBtn = document.querySelector(".sad");
+const fearBtn = document.querySelector(".fear");
 const drawBtn = document.querySelector(".draw");
 
 let gl;
 let dotGroup;
 let cube;
-let wave;
+let happy;
+let tornado;
+let anger;
+let sad;
+let fear;
+
 let pixelRatio;
 let stageWidth;
 let stageHeight;
@@ -18,7 +27,11 @@ let drawingMode = false;
 let curMode = "random";
 let onRandom = true;
 let onCube = false;
-let onWave = false;
+let onTornado = false;
+let onHappy = false;
+let onAnger = false;
+let onSad = false;
+let onFear = false;
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
@@ -26,7 +39,7 @@ function setup() {
 
 	pixelRatio = pixelDensity();
 
-	dotGroup = new DotGroup(pow(10, 3));
+	dotGroup = new DotGroup(pow(15, 3));
 
 	windowResized();
 
@@ -44,7 +57,10 @@ function windowResized() {
 
 function draw() {
 	if (!drawingMode) {
-		gl.clearColor(0.16, 0.33, 0.64, 0);
+		// gl.clearColor(0.16, 0.33, 0.64, 0);
+		gl.clearColor(0.1, 0.1, 0.1, 0);
+		// gl.clearColor(0.72, 0.91, 0.58, 1);
+		// gl.clearColor(0.6, 0.91, 0.61, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 	}
 	// createGraphics(500, 500, WEBGL);
@@ -59,8 +75,20 @@ const modeChange = (mode) => {
 		case "cube":
 			onCube = !onCube;
 			break;
-		case "wave":
-			onWave = !onWave;
+		case "tornado":
+			onTornado = !onTornado;
+			break;
+		case "happy":
+			onHappy = !onHappy;
+			break;
+		case "anger":
+			onAnger = !onAnger;
+			break;
+		case "sad":
+			onSad = !onSad;
+			break;
+		case "fear":
+			onFear = !onFear;
 			break;
 	}
 	curMode = mode;
@@ -80,17 +108,73 @@ const changeToCube = () => {
 	}
 };
 
-const changeToWave = () => {
-	if (onWave) {
-		onWave = !onWave;
+const changeToHappy = () => {
+	if (onHappy) {
+		onHappy = !onHappy;
 		onRandom = !onRandom;
 		curMode = "random";
 	} else {
-		onWave = !onWave;
-		modeChange("wave");
+		onHappy = !onHappy;
+		modeChange("happy");
 	}
-	if (!wave) {
-		wave = new Wave(dotGroup);
+	if (!happy) {
+		happy = new Happy(dotGroup);
+	}
+};
+
+const changeToSad = () => {
+	if (onSad) {
+		onSad = !onSad;
+		onRandom = !onRandom;
+		curMode = "random";
+	} else {
+		onSad = !onSad;
+		modeChange("sad");
+	}
+	if (!sad) {
+		sad = new Sad(dotGroup);
+	}
+};
+
+const changeToTornado = () => {
+	if (onTornado) {
+		onTornado = !onTornado;
+		onRandom = !onRandom;
+		curMode = "random";
+	} else {
+		onTornado = !onTornado;
+		modeChange("tornado");
+	}
+	if (!tornado) {
+		tornado = new Tornado(dotGroup);
+	}
+};
+
+const changeToAnger = () => {
+	if (onAnger) {
+		onAnger = !onAnger;
+		onRandom = !onRandom;
+		curMode = "random";
+	} else {
+		onAnger = !onAnger;
+		modeChange("anger");
+	}
+	if (!anger) {
+		anger = new Anger(dotGroup);
+	}
+};
+
+const changeToFear = () => {
+	if (onFear) {
+		onFear = !onFear;
+		onRandom = !onRandom;
+		curMode = "random";
+	} else {
+		onFear = !onFear;
+		modeChange("fear");
+	}
+	if (!fear) {
+		fear = new Fear(dotGroup);
 	}
 };
 
@@ -98,6 +182,11 @@ const OnDrawingMode = () => {
 	drawingMode = !drawingMode;
 };
 
-cubeBtn.addEventListener("click", changeToCube);
-waveBtn.addEventListener("click", changeToWave);
 drawBtn.addEventListener("click", OnDrawingMode);
+
+cubeBtn.addEventListener("click", changeToCube);
+tornadoBtn.addEventListener("click", changeToTornado);
+happyBtn.addEventListener("click", changeToHappy);
+angerBtn.addEventListener("click", changeToAnger);
+sadBtn.addEventListener("click", changeToSad);
+fearBtn.addEventListener("click", changeToFear);
